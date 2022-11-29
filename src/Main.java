95cc5bafd33c762da58c0cc9b5d836811747559e
Khadijha01 <luqmankhadijah24@gmail.com>
2022-11-29 06:44:10 +0100

    added first java project T14

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world! .This is my project");
+        System.out.println("My name is khadijah luqman");
+        System.out.println("i am a software tester");
+        System.out.println("this is my first java project");
+
+
+    }
+}
+
